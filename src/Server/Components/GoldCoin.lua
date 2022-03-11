966754daddcec7464586962a0c443608badb510e
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-11 11:21:26 -0600

    Add CoinComponent touched listener, and primary part existance assertion

--- src/Server/Components/GoldCoin.lua
+++ src/Server/Components/GoldCoin.lua
@@ -1,17 +1,43 @@
-local Coins = {}
-Coins.__index = Coins
+--[[
+    Gold coin component for instances that represent gold coins that reward the player
+    when he touches them
+]]
 
+local CollectionService = game:GetService("CollectionService")
 
-function Coins.new()
-    local self = setmetatable({}, Coins)
+local GoldCoin = {}
+GoldCoin.__index = GoldCoin
 
+function GoldCoin.new(instance: Model)
+    local self = setmetatable({}, GoldCoin)
+
+    if instance.PrimaryPart == nil then
+        local newPrimaryPart: Part = Instance.new("Part")
+
+        instance.PrimaryPart  = newPrimaryPart
+        newPrimaryPart.Parent = instance
+        
+        warn("No primary part was found, A default one was created")
+    end
+    
+    self.Instance = instance
+    self.RootPart = self.Instance.PrimaryPart
+    CollectionService:AddTag(self.Instance, "GoldCoin")
     return self
 end
 
+function GoldCoin:Init()
+    self.RootPart.Touched:Connect(function(theTouchedPart) 
+        self:Destroy()
+    end)
 
-function Coins:Destroy()
-    
+end
+
+function GoldCoin:Destroy()
+    print("Destroying")
+    self.Instance:Destroy()
+    self = nil
 end
 
 
-return Coins
+return GoldCoin
--- src/Server/Main.server.lua
+++ src/Server/Main.server.lua
@@ -2,11 +2,14 @@
     Main script:
     This is were the core loop of the game is ran, all server handler functions are called from this script
 ]]
-local Handlers = game:GetService("ServerScriptService").Handlers
-
+local Handlers   = game:GetService("ServerScriptService").Handlers
+local Components = game:GetService("ServerScriptService").Components
 --# <|=============== Dependencies ===============|>
+-- Handlers
 local PlayerDataHandler: ModuleScript = require(Handlers.PlayerData)
 
+-- Components
+local GoldCoinComponent: ModuleScript = require(Components.GoldCoin)
 
 --# <|=============== Services ===============|>
 local Players = game:GetService("Players")
@@ -28,3 +31,6 @@ Players.PlayerAdded:Connect(function(player:Player)
     stats.Parent = player
 
 end)
+
+local gc = GoldCoinComponent.new(workspace.Coin)
+gc:Init()
