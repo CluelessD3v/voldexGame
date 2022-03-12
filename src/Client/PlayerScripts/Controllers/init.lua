53fdfd87fb638a4721b7fc13ceb5a19be6d4eaae
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-12 13:44:10 -0600

    Add basic state interface to PlayerCombatClient

--- src/Client/PlayerScripts/Controllers/init.lua
+++ src/Client/PlayerScripts/Controllers/init.lua
@@ -4,12 +4,21 @@ PlayerCombatClient.__index = PlayerCombatClient
 
 function PlayerCombatClient.new()
     local self = setmetatable({}, PlayerCombatClient)
+    
+
+    self.States = {
+
+    }
+
+    self.CurrentState = nil
     return self
 end
 
 
-function PlayerCombatClient:Destroy()
-    
+function PlayerCombatClient:SwitchState(newState: ModuleScript)
+    self.CurrentState:Exit()
+    self.CurrentState = newState.new()
+    self.CurrentState:Start()
 end
 
 
