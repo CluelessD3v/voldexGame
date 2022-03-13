99ff1b309efd3ebb5e17d358c64e3711b6a98827
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-13 12:08:53 -0500

    Add Trove to idle state

--- src/shared/Controllers/PlayerCombatClient/Idle.lua
+++ src/shared/Controllers/PlayerCombatClient/Idle.lua
@@ -1,5 +1,8 @@
 --# <|=============== SERVICES ===============|>
 local UserInputService = game:GetService("UserInputService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+--# <|=============== DEPENDENCIES ===============|>
+local Trove = require(ReplicatedStorage.Packages.trove)
 
 local Idle = {}
 Idle.__index = Idle
@@ -9,21 +12,21 @@ function Idle.new(context: table)
     local self = setmetatable({}, Idle)
     self.Context = context
     self.Name = "Idle"
+    self.Trove = context.Trove:Extend() --# Constructs a new trove inside the context trove
 
-    self.ActionOneConn = nil
     return self
 end
 
 function Idle:Start()
-    self.ActionOneConn = UserInputService.InputBegan:Connect(function(io: InputObject, busy: boolean )
+    self.Trove:Add(UserInputService.InputBegan:Connect(function(io: InputObject, busy: boolean )
         if io.UserInputType == Enum.UserInputType.MouseButton1 and not busy then
             self.Context:SwitchState(self.Context.States.CastingActionOne)
         end
-    end)
+    end))
 end
 
 function Idle:Exit()
-    self.ActionOneConn:Disconnect()
+    self.Trove:Clean()
 end
 
 
