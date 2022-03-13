05b4c8ad88b0305e87b25b5060e63b9dab9dbcf3
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-12 21:58:43 -0600

    Add API comments to PlayerCombatClient

--- src/shared/Controllers/PlayerCombatClient/init.lua
+++ src/shared/Controllers/PlayerCombatClient/init.lua
@@ -1,6 +1,14 @@
+--[[
+    This controller serves as the context class for the concrete combat states it requires.
+    It allows combat states to transition between each other, and be a data repo for data they might 
+    require to function.
+
+    It also holds an internal reference to the player(Host) it is current operating in
+]]
+--# <|=============== SERVICES ===============|>
 local Players = game:GetService("Players")
 
-
+--? <|=============== CONSTRUCTOR ===============|>
 local PlayerCombatClient = {}
 PlayerCombatClient.__index = PlayerCombatClient
 
@@ -9,6 +17,7 @@ function PlayerCombatClient.new()
     local self = setmetatable({}, PlayerCombatClient)
     self.Host = Players.LocalPlayer
 
+    --# Concrete states the context manages
     self.States = {
         CastingActionOne = require(script.CastingActionOne),
         CastingActionTwo = require(script.CastingActionTwo),
@@ -19,23 +28,32 @@ function PlayerCombatClient.new()
     return self
 end
 
+
+--+ <|=============== PUBLIC FUNCTIONS ===============|>
+--* KickStarts PlayerCombat State machine & allows players to engage with the combat system
 function PlayerCombatClient:Start()
     self:SwitchState(self.States.Idle)
 end
 
+
+--* Exits from the combat system
 function PlayerCombatClient:Exit()
     self.CurrentState:Exit()
     self.CurrentState = nil
 end
 
+--* Context Interface function that allows Required Concrete states to transition Between each other 
 
 function PlayerCombatClient:SwitchState(newState: ModuleScript)
+    --# Does the Current state object exist? Great
+    --# transition out of said current state, set 
+    --# new state as current & start it else default to idle
+
     if self.CurrentState then
         self.CurrentState:Exit()
         self.CurrentState = newState.new(self)
         print(self.CurrentState.Name)
         self.CurrentState:Start()
-        
         return
     end
 
