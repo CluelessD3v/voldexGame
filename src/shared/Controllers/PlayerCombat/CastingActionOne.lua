f47d70a82ca156079b354a4538150b03b15fe99a
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-15 14:09:04 -0500

    Add condition to see if touched part parent has Dragon tag in CastingActionOne state

--- src/shared/Controllers/PlayerCombat/CastingActionOne.lua
+++ src/shared/Controllers/PlayerCombat/CastingActionOne.lua
@@ -1,6 +1,6 @@
 --# <|=============== SERVICES ===============|>
 local Players = game:GetService("Players")
-
+local CollectionService = game:GetService("CollectionService")
 
 --? <|=============== CONSTRUCTOR ===============|>
 local CastingActionOne = {}
@@ -20,7 +20,14 @@ end
 --+ <|=============== PUBLIC FUNCTIONS ===============|>
 function CastingActionOne:Start()
     self.Trove:Add(self.Context.EquippedWeapon.Handle.Touched:Connect(function(theTouchedPart)
-        print(theTouchedPart)
+        if CollectionService:HasTag(theTouchedPart.Parent, "Dragon") then
+            local humanoid = theTouchedPart.Parent:FindFirstChild("Humanoid")
+
+            if humanoid then
+                print(humanoid)
+            end
+        end
+
     end))
 
     local humanoid = Players.LocalPlayer.Character.Humanoid
@@ -36,7 +43,10 @@ function CastingActionOne:Start()
 
     local animationTrack: AnimationTrack = animator:LoadAnimation(animationsList[self.Context.ComboCount])
     animationTrack:Play()
-
+    --*//TODO do a speed adjustment to make animations more responsive, in fact, look into speeding them realtive to combo count
+    
+    
+    animationTrack.Stopped:Wait()
     self.Context:SwitchState(self.Context.States.Idle)
 end
 
