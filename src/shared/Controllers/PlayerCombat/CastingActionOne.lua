c9ee5693ea41ee90990c87b3be540749db60ce19
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-15 13:01:32 -0500

    Add EquippedWeapon Touched connection to trove in CastingActionOne state

--- src/shared/Controllers/PlayerCombat/CastingActionOne.lua
+++ src/shared/Controllers/PlayerCombat/CastingActionOne.lua
@@ -4,22 +4,26 @@ CastingActionOne.__index = CastingActionOne
 
 function CastingActionOne.new(context)
     local self = setmetatable({}, CastingActionOne)
+
     self.Name = "CastingActionOne"
     self.Context = context
     self.Trove = context.Trove:Extend()
+
     return self
 end
 
 function CastingActionOne:Start()
-    self.Context.EquippedWeapon.Handle.Touched:Connect(function(theTouchedPart)
+    self.Trove:Add(self.Context.EquippedWeapon.Handle.Touched:Connect(function(theTouchedPart)
         print(theTouchedPart)
-    end)
+    end))
+    
     self.Context:SwitchState(self.Context.States.Idle)
 end
 
 
 
 function CastingActionOne:Exit()
+    self.Trove:Clean()
     return
 end
 
