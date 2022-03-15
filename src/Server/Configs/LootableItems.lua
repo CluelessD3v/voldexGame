b15cbc41cb70e6cff00e74e6a2bfbf89e143759d
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-15 00:10:20 -0500

    Add API comments to PlayerInventory

--- src/Server/Configs/LootableItems.lua
+++ src/Server/Configs/LootableItems.lua
@@ -1,10 +1,11 @@
 return {
     SwordType = {
         ClassicSword = {
-            Name             = "ClassicSword",
-            LootableInstance = workspace.ClassicSword,
-            ToolInstance     = workspace.Swords.ClassicSword,
-            Tags             = {},
+            Name                 = "ClassicSword",
+            LootableInstance     = workspace.ClassicSword,
+            ToolInstance         = workspace.Swords.ClassicSword,
+            LootableInstanceTags = {},
+            ToolInstanceTags     = {},
             
             Attributes = {
                 Damage = 25
--- src/Server/Handlers/PlayerInventory.lua
+++ src/Server/Handlers/PlayerInventory.lua
@@ -24,15 +24,41 @@ function PlayerInventory.new()
     return self
 end
 
-function PlayerInventory:BuildItemIntoPlayerBackpack(player: Player, theLootedItem: Part | MeshPart, itemData: table)
-    if theLootedItem.Name == itemData.Name then
-        local newItem: Tool = itemData.ToolInstance:Clone()
+
+--* Does a simple 1:1 "conversion" from part/mesh part to a tool, the given looted name MUST match the itemTypeData name field
+--* Else the function will not even consider the item
+
+function PlayerInventory:BuildItemIntoPlayerBackpack(player: Player, theLootedItem: Part | MeshPart, itemTypeData: table)
+    itemTypeData.Attributes       = itemTypeData.Attributes or {}
+    itemTypeData.ToolInstanceTags = itemTypeData.ToolInstanceTags or {}
+
+    if theLootedItem.Name == itemTypeData.Name then
+        local newItem: Tool = itemTypeData.ToolInstance:Clone()
         newItem.Parent = player.Backpack
         return 
     end
     warn("The given looted item name does not match any in the given Item list! Failed to build Item")
 end
 
+
+--[[
+    itemTypeData Interface,
+    These are the fields the itemTypeData table should have to be able to do a conversion,
+    fields marked with a ? are optional:
+
+    Name              : string         = Non unique identifier to match the looted item with the itemTypeData,
+    ToolInstanc       : Tool           = Instance reference that the function will clone to the player backpack,
+    LootableInstance?: Part | MeshPart = Instance reference that will serve as the "lootable" instance in the workspace
+                                         Useful to build a LootableItemEntity!
+
+    Attributes?: table             = table of attributes that can be mapped to both the LootableInstance and the tool instance
+    ToolInstanceTagsTags? : table  = table of tags that are mapped to the ToolInstance when it is built
+    LootableInstanceTags?: tablle  =  table of tags that are mapped to the LootableInstance when it is constructed 
+                                      (LootableItemEntity related)
+]]--
+
+--* Fires the WeaponEquipped bindable event when a child with the "Weapon" tag is added to the given character
+--* Note that is does not need to be a tool necesarilly, it just needs the "Weapon tag"
 function PlayerInventory:TrackIfCharacterEquippedWeapon(character: Model)
     character.ChildAdded:Connect(function(child)
         if CollectionService:HasTag(child, "Weapon") then
