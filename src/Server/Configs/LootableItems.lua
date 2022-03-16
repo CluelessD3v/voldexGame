dd75a304b6c853c47f7d5a814a0827924fb8ec12
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-16 11:56:06 -0500

    Refactor LootableItems to now have 2 table fields, DisplayItem & ToolItem

--- src/Server/Configs/LootableItems.lua
+++ src/Server/Configs/LootableItems.lua
@@ -1,6 +1,19 @@
 return {
     SwordType = {
         ClassicSword = {
+            DisplayItem = {
+                Name       = "ClassicSword",
+                Instance   = workspace.ClassicSword,
+            },
+
+            ToolItem = {
+                Name       = "ClassicSword",
+                Instance   = workspace.Swords.ClassicSword,
+                Attributes = {
+                    Damage = 25
+                }
+            },
+
             Name                 = "ClassicSword",
             LootableInstance     = workspace.ClassicSword,
             ToolInstance         = workspace.Swords.ClassicSword,
