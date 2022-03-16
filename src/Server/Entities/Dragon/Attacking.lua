4d64bd1cc507c913a894a02019d7bd665fbf4494
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-15 19:46:50 -0500

    Add Attack DragonEnitity Attack state module

---
+++ src/Server/Entities/Dragon/Attacking.lua
@@ -0,0 +1,26 @@
+local Attacking = {}
+Attacking.__index = Attacking
+
+
+function Attacking.new(context: table)
+    local self = setmetatable({}, Attacking)
+
+    self.Name = "Attacking"
+    self.Context = context
+    self.Trove = context.Trove:Extend()
+
+
+    return self
+end
+
+function Attacking:Start()
+
+end
+
+function Attacking:Exit()
+    self.Trove:Clean()
+    return
+end
+
+
+return Attacking
