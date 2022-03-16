e1ecd5bec55197392edf382fabce1ce59d393f67
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-16 13:32:10 -0500

    Remove Old Data structure for LootableItems

--- src/Server/Configs/LootableItems.lua
+++ src/Server/Configs/LootableItems.lua
@@ -22,16 +22,6 @@ return {
                     Damage = 25
                 }
             },
-
-            Name                 = "ClassicSword",
-            LootableInstance     = workspace.ClassicSword,
-            ToolInstance         = workspace.Swords.ClassicSword,
-            LootableInstanceTags = {},
-            ToolInstanceTags     = {},
-            
-            Attributes = {
-                Damage = 25
-            },
         },
 
 
