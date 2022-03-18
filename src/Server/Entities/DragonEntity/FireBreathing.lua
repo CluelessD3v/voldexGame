e07dbff769306a4bb59aa6de318f7876297ef8e3
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-18 00:39:29 -0500

    Add FireBreathing state module

---
+++ src/Server/Entities/DragonEntity/FireBreathing.lua
@@ -0,0 +1,26 @@
+local FireBreathing = {}
+FireBreathing.__index = FireBreathing
+
+
+function FireBreathing.new(context: table)
+    local self = setmetatable({}, FireBreathing)
+
+    self.Name    = "FireBreathing"
+    self.Context = context
+    self.Instance = self.Context.Instance
+    self.Trove   = context.Trove:Extend()
+    
+    return self
+end
+
+function FireBreathing:Start()
+    
+end
+
+
+function FireBreathing:Exit()
+    self.Trove:Clean()
+end
+
+
+return FireBreathing
