23a713dc299426b02b074107b39581d9202ee3a4
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-18 01:11:48 -0500

    Add 2 seconds timer to PreparingAttack state so dragon takes some time between attacks

--- src/Server/Entities/DragonEntity/PreparingAttack.lua
+++ src/Server/Entities/DragonEntity/PreparingAttack.lua
@@ -1,7 +1,11 @@
+--[[
+    Dragon Entty state: In this state the dragon instance takes a momment to act
+
+]]
 --# <|=============== SERVICES ===============|>
 local RunService = game:GetService("RunService")
 
-
+--? <|=============== CONSTRUCTOR ===============|>
 local PreparingAttack = {}
 PreparingAttack.__index = PreparingAttack
 
@@ -18,6 +22,7 @@ function PreparingAttack.new(context: table)
     return self
 end
 
+--+ <|=============== PUBLIC FUNCTIONS ===============|>
 function PreparingAttack:Start()
     self.Instance.Humanoid.Died:Connect(function()
         self:Exit()
@@ -26,26 +31,28 @@ function PreparingAttack:Start()
     print("entered attack")
 
     self.Context.AnimationTracks.Idle:Play()
-    self.Context.AnimationTracks.Idle:AdjustSpeed(4)
+    self.Context.AnimationTracks.Idle:AdjustSpeed(2.5)
 
     self.Instance.Humanoid:MoveTo(self.Instance.PrimaryPart:GetPivot().Position)
     
     local n = math.random(1, 2)
+    local prepareTime = 2.5
+    local startedPreparing = time()
+
 
     self.Trove:Add(RunService.Heartbeat:Connect(function()
-        if not self.Context:TaggedInstanceEnteredAttackAgro() then
-            self.Context:SwitchState(self.Context.States.ChasingPlayer)
-        else
-            if n == 1 then
-                self.Context:SwitchState(self.Context.States.WingBeating)
+        if (time() - startedPreparing) >= prepareTime then
+            if not self.Context:TaggedInstanceEnteredAttackAgro() then
+                self.Context:SwitchState(self.Context.States.ChasingPlayer)
             else
-                self.Context:SwitchState(self.Context.States.FireBreathing)
+                if n == 1 then
+                    self.Context:SwitchState(self.Context.States.WingBeating)
+                else
+                    self.Context:SwitchState(self.Context.States.FireBreathing)
+                end
             end
-
         end
     end))
-
-    
 end
 
 function PreparingAttack:Exit()
