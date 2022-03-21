b95fe1121daf87dd8e949eee4b21b0f6d131dd9a
CluelessD3v <cluelessdevmail@gmail.com>
2022-03-20 21:32:45 -0500

    Add Health counter to HUD controller, that tracks dragon health in text

--- src/Client/PlayerScripts/HudController.client.lua
+++ src/Client/PlayerScripts/HudController.client.lua
@@ -9,28 +9,34 @@ local WorldEvents               = ReplicatedStorage.Events.WorldRunner
 local dragonDied                = WorldEvents.DragonDied
 local playerEnteredCurrentLevel = WorldEvents.PlayerEnteredCurrentLevel
 
-local worldData: Folder = Workspace.WorldData
+local worldData: Folder               = Workspace.WorldData
 local dragonLevelObject: NumberValue  = worldData.DragonLevel
 local dragonHealthObject: NumberValue = worldData.DragonHealth
-local dragonMaxHealth: NumberValue    = worldData.DragonMaxHealth
+local dragonMaxHealthObject: NumberValue    = worldData.DragonMaxHealth
+local dragonNameObject : StringValue        = worldData.DragonName
 
 local PlayerGui: PlayerGui = Players.LocalPlayer.PlayerGui 
 
-local hud                            = PlayerGui:WaitForChild("HUD")
-local dragonStatsFrame: Frame        = hud:WaitForChild("DragonStatsFrame")
-local dragonHealthBackground: Frame  = dragonStatsFrame:WaitForChild("DragonHealthBackground")
-local dragonHealthDisplay: Frame     = dragonHealthBackground:WaitForChild("DragonHealthDisplay")
-local HealthCounter: TextLabel = dragonHealthDisplay:WaitForChild("HealthCounter")
+local hud                           = PlayerGui:WaitForChild("HUD")
+local dragonStatsFrame: Frame       = hud:WaitForChild("DragonStatsFrame")
+local dragonHealthBackground: Frame = dragonStatsFrame:WaitForChild("DragonHealthBackground")
+local dragonHealthDisplay: Frame    = dragonHealthBackground:WaitForChild("DragonHealthDisplay")
+local healthCounter: TextLabel      = dragonHealthBackground:WaitForChild("HealthCounter")
 
 -- local healthCounter: TextLabel = dragonStatsFrame:WaitForChild("HealthCounter")
 
 local DragonHealthUpdateConn 
 dragonStatsFrame.Visible = false
+
 playerEnteredCurrentLevel.OnClientEvent:Connect(function()
-    dragonStatsFrame.Visible = true
+    
     DragonHealthUpdateConn = RunService.RenderStepped:Connect(function()
-        dragonHealthDisplay.Size = UDim2.fromScale(dragonHealthObject.Value/dragonMaxHealth.Value, 1)
+        dragonHealthDisplay.Size = UDim2.fromScale(dragonHealthObject.Value/dragonMaxHealthObject.Value, 1)
+        healthCounter.Text = tostring(dragonMaxHealthObject.Value.."/"..dragonHealthObject.Value)
     end)
+
+    dragonStatsFrame.Visible = true
+
 end)
 
 
